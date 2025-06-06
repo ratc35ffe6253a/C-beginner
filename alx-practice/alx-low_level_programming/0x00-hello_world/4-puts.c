ff2b79fed4de1bcf21f4e8ff8e67320c655f31d7
specialization24 <wangalibosamuel@yahoo.com>
2025-06-06 07:01:32 +0300

    C

---
+++ alx-practice/alx-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints out a string to standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ alx-practice/alx-low_level_programming/0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - size of a type of variable in bytes
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	sizeof(c);
+	return (0);
+}
