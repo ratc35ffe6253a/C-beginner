28f0e649295a2072805d646700731620a98f3180
specialization24 <wangalibosamuel@yahoo.com>
2025-06-11 13:20:18 +0300

    Positive anything is better than negative nothing

--- alx-practice/alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,14 +13,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if (n == 0)
+	}
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
