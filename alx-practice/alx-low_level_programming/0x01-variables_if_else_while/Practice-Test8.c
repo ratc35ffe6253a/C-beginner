a24791249d8409849f58e9e2627e4b709212dd3d
specialization24 <wangalibosamuel@yahoo.com>
2025-06-12 13:25:16 +0300

    Reverse Lowercase Print

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/Practice-Test8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+
+	return (0);
+}
