7e13f0b389afa3ebd6905030dc389b3eebdb906f
specialization24 <wangalibosamuel@yahoo.com>
2025-06-12 11:33:10 +0300

    Last Digit Extractor

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/Practice-Test5.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = abs(n % 10);
+
+	printf("Last digit of %d is %d\n", n, last_digit);
+
+	return (0);
+}
