1b7cf9a1889b7508bd54e382caacefcf0c372348
specialization24 <wangalibosamuel@yahoo.com>
2025-06-23 15:30:02 +0300

    Add the Newline

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet-Practice/Practice-Test2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		printf("%c", letter++);
+	}
+	printf("\n");
+
+	return (0);
+}
