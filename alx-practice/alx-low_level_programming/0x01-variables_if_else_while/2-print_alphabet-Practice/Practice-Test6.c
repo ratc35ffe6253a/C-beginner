52fb11ab1c2d09b14dc427c203af02e8dd987eb7
specialization24 <wangalibosamuel@yahoo.com>
2025-06-24 03:42:52 +0300

    Print All Digits

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet-Practice/Practice-Test6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number++);
+	}
+	putchar('\n');
+
+	return (0);
+}
