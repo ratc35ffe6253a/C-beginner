b1d2bca70765f506eaeaf3210ed87b0b78751965
specialization24 <wangalibosamuel@yahoo.com>
2025-07-19 14:49:44 +0300

    🧪 Practice Test 8: Print 0 to 9, but after 5, insert a space ' ' before continuing.

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/6-print_numberz-Practice/Practice-Test8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num == 5)
+		{
+			putchar(num + '0');
+			putchar(' ');
+		}
+		else if (num != 5)
+		{
+			putchar(num + '0');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
