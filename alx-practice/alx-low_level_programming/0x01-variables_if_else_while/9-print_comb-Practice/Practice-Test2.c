3406b23a881d2f008b503ed97033b10fac4a1220
specialization24 <wangalibosamuel@yahoo.com>
2025-07-24 19:26:05 +0300

    ✅ Practice Test 2 – Even digits only with separator (no trailing comma)

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/9-print_comb-Practice/Practice-Test2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(i + '0');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
