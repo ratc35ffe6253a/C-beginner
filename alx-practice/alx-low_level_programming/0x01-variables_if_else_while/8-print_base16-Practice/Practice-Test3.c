ffa1ec379210624a70fc997c6f3dd9ea5ff6db31
specialization24 <wangalibosamuel@yahoo.com>
2025-07-24 16:52:08 +0300

    Practice 3: Print base 16 in lowercase, but:Skip letters b and e

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/8-print_base16-Practice/Practice-Test3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	char letter = 'a';
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		if (letter != 'b' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
