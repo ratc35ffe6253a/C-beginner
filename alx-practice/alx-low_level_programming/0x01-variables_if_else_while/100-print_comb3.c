b0761f59640d0f08059414d9effbaa8e6b0ad9e9
specialization24 <wangalibosamuel@yahoo.com>
2025-08-08 20:52:40 +0300

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a = 0, b;
+
+    while (a <= 8)
+    {
+        b = a + 1;
+        while (b <= 9)
+        {
+            putchar(a + '0');
+            putchar(b + '0');
+
+            if (!(a == 8 && b == 9))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            b++;
+        }
+        a++;
+    }
+    putchar('\n');
+    return (0);
+}
+
