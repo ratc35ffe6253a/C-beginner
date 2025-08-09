c8bf2559d896cbc76717e2e11542140d45a6ef88
specialization24 <wangalibosamuel@yahoo.com>
2025-08-09 15:09:12 +0300

    🧪 Practice Test 2: Odd-First Triples.

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/101-print_comb4-Practice/Practice-Test2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b;
+	int c;
+
+	while (a <= 9)
+	{
+		if (a % 2 != 0)
+		{
+			b = a + 1;
+			while (b <= 9)
+			{
+				c = b + 1;
+				while (c <= 9)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if (!(a == 7 && b == 8 && c == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					c++;
+				}
+				b++;
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
