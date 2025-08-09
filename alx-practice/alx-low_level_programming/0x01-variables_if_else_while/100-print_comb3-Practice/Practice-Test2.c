0af0e5ad1da5319e31e564a943488a29943b8412
specialization24 <wangalibosamuel@yahoo.com>
2025-08-09 12:18:12 +0300

    🧪 Practice Test 2: Odd + Even Pairs

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/100-print_comb3-Practice/Practice-Test2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b;
+
+	while (a <= 9)
+	{
+		if (a % 2 != 0)
+		{
+			b = 0;
+			
+			while (b <= 9)
+			{
+				if (b % 2 == 0 && b > a)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					
+					if (!(a == 7 && b == 8))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				b++;
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
