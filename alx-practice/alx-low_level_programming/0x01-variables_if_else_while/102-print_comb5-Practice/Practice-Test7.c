4ddbd12dfabf2863e84a454cd16a173a0099b1cc
specialization24 <wangalibosamuel@yahoo.com>
2025-08-26 12:59:23 +0300

    🔹 Practice Test 7: Pairs with Sum Divisible by 5 && 🔹 Practice Test 8: Unique Pairs with Difference Divisible by 3

---
+++ alx-practice/alx-low_level_programming/0x01-variables_if_else_while/102-print_comb5-Practice/Practice-Test7.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1 = 0;
+	int num2, sum;
+	int first = 1;
+
+	while (num1 <= 98)
+	{
+		num2 = num1 + 1;
+
+		while (num2 <= 99)
+		{
+			sum = num1 + num2;
+			if (sum % 5 == 0)
+			{
+				if (!first)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				putchar(num1 / 10 + '0');
+				putchar(num1 % 10 + '0');
+				putchar(' ');
+				putchar(num2 / 10 + '0');
+				putchar(num2 % 10 + '0');
+
+				first = 0;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
